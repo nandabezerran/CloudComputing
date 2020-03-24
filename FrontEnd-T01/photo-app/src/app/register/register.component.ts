1ad2aeecb68d8054dabd0d173b650ac6307473d9
ArinaSanches <sanchesarina96@gmail.com>
2020-03-24 15:16:28 -0300

    Adição de verificações

--- FrontEnd-T01/photo-app/src/app/register/register.component.ts
+++ FrontEnd-T01/photo-app/src/app/register/register.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  public name: string;
+  public email: string;
+  public userName: string;
+  public password: string;
+  public profilePhoto: File;
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,17 +15,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  model = new User();
+
   constructor() { }
+
+  onSubmit(form) {
+    console.log(form.value)
+  }
 
   ngOnInit(): void {
   }
-
-  Hobbies: string[] = [
-    'Acrobatics',
-    'Acting',
-    'Animation',
-    'Astronomy',
-    'Baking'
-  ];
 
 }
