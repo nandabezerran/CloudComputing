71e69bdebfba29c62bd71f2bb2c2bfb3fc950a60
ArinaSanches <sanchesarina96@gmail.com>
2020-03-31 14:10:56 -0300

    cotinuacao

--- FrontEnd-T01/photo-app/src/app/profile/profile-card/profile-card.component.ts
+++ FrontEnd-T01/photo-app/src/app/profile/profile-card/profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileCardComponent implements OnInit {
 
+  @Input() data: any;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  likePhoto(): void{
+    this.data.youLiked = !this.data.youLiked;
+    if(this.data.youLiked){
+      this.data.likes++;
+    }
+    else{
+      this.data.likes--;
+    }
   }
 
 }
