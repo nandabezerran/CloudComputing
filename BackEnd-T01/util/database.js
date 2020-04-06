fd095add711a464dbb798eb4c8d386f17bd46e05
Pedro <pedroln97@gmail.com>
2020-04-06 20:32:01 -0300

    Adicionando database atualizada

--- BackEnd-T01/util/database.js
+++ BackEnd-T01/util/database.js
@@ -10,7 +10,7 @@ let _db;
 //Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set, 
 //  and specify the read preference as secondary preferred
 var client = MongoClient.connect(
-'mongodb://photoAppData:<insertYourPassword>@docdb-2020-04-05-18-39-37.cluster-clmniwzqtwwj.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
+'mongodb://photoAppData:cloud159@docdb-2020-04-05-18-39-37.cluster-clmniwzqtwwj.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
 { 
   sslValidate: true,
   sslCA:ca,
@@ -33,7 +33,7 @@ const getDB = () => {
 }
 
 exports.mongoConnect = client;
-exports.getDB = getDB
+exports.getDB = getDB 
 
 /*const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
